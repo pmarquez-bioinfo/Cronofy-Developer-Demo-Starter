e2b9bf3df588096f6e380bd2e38eb9d86a6ec791
Pablo Marquez <pamarquez90@gmail.com>
2024-11-25 09:52:17 -0300

    featuer(added calendar sync)

--- app/server.js
+++ app/server.js
@@ -2,12 +2,20 @@
 const dotenv = require("dotenv");
 const express = require("express");
 const bodyParser = require("body-parser");
+const Cronofy = require("cronofy");
 
 // Enable dotenv
 dotenv.config();
 
 // Setup
 const PORT = 7070;
+
+// configure the Cronofy client
+const cronofyClient = new Cronofy({
+  client_id: process.env.CLIENT_ID,
+  client_secret: process.env.CLIENT_SECRET,
+  data_center: process.env.DATA_CENTER,
+});
 
 // Setup Express
 const app = express();
@@ -19,34 +27,75 @@ app.use(express.static(__dirname + "/"));
 // Add the Cronofy client setup here
 
 // Route: home
-app.get("/", async (req, res) => {
-    // Homepage code goes here
+// app.get("/", async (req, res) => {
+//   // Extract the "code" from the page's query string:
+//   const codeFromQuery = req.query.code;
+//
+//   if (codeFromQuery) {
+//     const codeResponse = await cronofyClient
+//       .requestAccessToken({
+//         client_id: process.env.CLIENT_ID,
+//         client_secret: process.env.CLIENT_SECRET,
+//         grant_type: "authorization_code",
+//         code: codeFromQuery,
+//         redirect_uri: "http://localhost:7070",
+//       })
+//       .catch((err) => {
+//         console.error(err);
+//       });
+//
+//     console.log(codeResponse);
+//   }
+//
+//   // ...template rendering
+//   return res.render("home", {
+//     client_id: process.env.CLIENT_ID,
+//     data_center: process.env.DATA_CENTER,
+//   });
+// });
 
-    return res.render("home", {
-        token: "YOUR_TOKEN_GOES_HERE",
-        client_id: process.env.CLIENT_ID,
+app.get("/", async (req, res) => {
+  // Add this:
+  const token = await cronofyClient
+    .requestElementToken({
+      version: "1",
+      permissions: ["account_management"],
+      subs: [process.env.SUB],
+      origin: "http://localhost:7070",
+    })
+    .catch((err) => {
+      console.error(err);
     });
+
+  // ...code-redemption section remains unchanged
+
+  return res.render("home", {
+    // Add the element_token to our template:
+    element_token: token.element_token.token,
+    client_id: process.env.CLIENT_ID,
+    data_center: process.env.DATA_CENTER,
+  });
 });
 
 // Route: availability
 app.get("/availability", async (req, res) => {
-    // Availability code goes here
+  // Availability code goes here
 
-    return res.render("availability", {
-        token: "YOUR_TOKEN_GOES_HERE",
-        sub: process.env.SUB,
-    });
+  return res.render("availability", {
+    token: "YOUR_TOKEN_GOES_HERE",
+    sub: process.env.SUB,
+  });
 });
 
 // Route: submit
 app.get("/submit", async (req, res) => {
-    // Submit code goes here
+  // Submit code goes here
 
-    return res.render("submit", {
-        meetingDate: "MEETING_DATE_GOES_HERE",
-        start: "START_TIME_GOES_HERE",
-        end: "END_TIME_GOES_HERE",
-    });
+  return res.render("submit", {
+    meetingDate: "MEETING_DATE_GOES_HERE",
+    start: "START_TIME_GOES_HERE",
+    end: "END_TIME_GOES_HERE",
+  });
 });
 
 app.listen(PORT);
